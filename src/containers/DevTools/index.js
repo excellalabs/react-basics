4ef37440da7bf5029e0f9efdaddc655f484c8180
Kevin Groat <apps@kevingroat.io>
2017-06-21 12:51:08 -0400

    Split out devtools

--- src/containers/DevTools/index.js
+++ src/containers/DevTools/index.js
@@ -1,2 +1,8 @@
 
-export { default } from './DevTools';
+const prod = process.env.NODE_ENV === 'production';
+
+const DevTools = prod
+  ? require('./DevTools.prod').default
+  : require('./DevTools.dev').default;
+
+export default DevTools;
