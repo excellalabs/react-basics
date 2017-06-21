c6cd108d315391558cb2b292a598df7cfc0d9a0c
Kevin Groat <apps@kevingroat.io>
2017-06-21 09:45:32 -0400

    Added initial firebase impl

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import firebase from 'firebase';
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBlQgp_l69iQIlu5aw4itjU0kiiD9FE6v0",
+  authDomain: "chat-app-ad60c.firebaseapp.com",
+  databaseURL: "https://chat-app-ad60c.firebaseio.com",
+  projectId: "chat-app-ad60c",
+  storageBucket: "chat-app-ad60c.appspot.com",
+  messagingSenderId: "825235750540"
+});
+
+export default app;
